8c6dda5481b2f90ce0e1e724c380294f59428b04
Pri Adom <pri.adom1@gmail.com>
2024-02-05 01:59:59 +0000

    understanding Es6 arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,41 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 4);
+// console.log(sum);
 
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 4);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); - also valid
+
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3, 9);
+console.log(sum2);
+
 
 // Implicit Returns
 
+const sayingSomething = (message) => console.log(message);
+sayingSomething('Hello there!!');
+
+const sayHello = () => console.log('Hello');
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multi line string!
+    </p>`
+)
+console.log(returnMultipleLines());
